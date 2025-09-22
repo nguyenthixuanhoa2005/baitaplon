d19863301d36d2b04a49e2afce687653cc7a391b
Nguyen Thi Xuan Hoa <loveyourself24122005@gmail.com>
2025-09-22 11:06:29 +0700

    remove database file

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Quanlisachcoban
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new DangNhap());
+            Application.Run(new FormQuanLySach());
         }
     }
 
